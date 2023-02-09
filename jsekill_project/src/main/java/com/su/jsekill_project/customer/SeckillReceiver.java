f09d11263f27221828002dc742ec5853ccfaf7f6
hello@qq.com <hello>
2023-02-09 14:21:55 +0800

    消费者项目的项目

---
+++ jsekill_project/src/main/java/com/su/jsekill_project/customer/SeckillReceiver.java
@@ -0,0 +1,24 @@
+package com.su.jsekill_project.customer;
+
+import com.su.jsekill_project.dto.SeckillMsgBody;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @Classname SeckillReceiver
+ * @author: 我心
+ * @Description: 用于消费mq中秒杀队列的消息
+ * @Date 2023/2/9 14:15
+ * @Created by Lenovo
+ */
+@Component
+@RabbitListener(queues = "seckill_queue")
+public class SeckillReceiver {
+    @RabbitHandler
+    public  void  process(SeckillMsgBody msgBody){
+        System.out.println("接收到消息"+msgBody);
+    }
+}
